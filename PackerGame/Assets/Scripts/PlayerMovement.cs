80d2c2a525a076feebde1a77e71df4b84f32887a
Slin46 <slin46@pratt.edu>
2026-02-02 15:18:49 -0500

    speed

--- PackerGame/Assets/Scripts/PlayerMovement.cs
+++ PackerGame/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 4f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
