b631abfa1910e2e608e1853f1049c2996136c4bd
dyal <csuh4@pratt.edu>
2026-02-04 15:20:12 -0500

    coded item detecting function in Box script

--- PackerGame/Assets/Scripts/BoxScript.cs
+++ PackerGame/Assets/Scripts/BoxScript.cs
@@ -3,14 +3,30 @@ using UnityEngine.InputSystem;
 
 public class BoxScript : MonoBehaviour
 {
-    
+    public bool isInside = false;
+    public bool isPacked = false;
+    public Transform boxInteriorPoint; 
+    private ItemGenerator item;
     // Update is called once per frame
    void Update()
     {
         
     }
+    void OnTriggerEnter(Collider item)
+    {
+        // Check if the colliding object has the specific tag
+        if (item.gameObject.CompareTag("Grabbable"))
+        {
+            // If it matches, execute specific actions here
+            PackBox();
+            Debug.Log("Pack!");
+            // You can access other components or properties of the detected object using 'other.gameObject'
+            // other.gameObject.GetComponent<SomeOtherScript>().DoSomething();
+        }
+    }
     void PackBox()
     {
         // notify quest manager
     }
+        // A method to place an item inside the box
 }
