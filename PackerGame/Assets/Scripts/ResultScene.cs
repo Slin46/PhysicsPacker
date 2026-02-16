dc099ef3d06f96db0d5fc146e61507900831338c
dyal <csuh4@pratt.edu>
2026-02-16 00:31:37 -0500

    rewinded result screen

--- PackerGame/Assets/Scripts/ResultScene.cs
+++ PackerGame/Assets/Scripts/ResultScene.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 
 public class ResultScene : MonoBehaviour
 {
-    public AudioSource resultBgm;
     public TextMeshProUGUI resultText;
     public GameObject winImage;
     public GameObject loseImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        resultBgm = GetComponent<AudioSource>();
         string result = PlayerPrefs.GetString("RESULT", "LOSE");
         //color coding the result strings in hexcode
         ColorUtility.TryParseHtmlString("#578F2D", out Color winGreen); //green
@@ -19,7 +17,7 @@ public class ResultScene : MonoBehaviour
         //if result is win then it will say pay day in green and the win image will activate
         if (result == "WIN")
         {
-            resultBgm.Play();
+
             resultText.text = "PAY DAY!";
             resultText.color = winGreen;
             winImage.SetActive(true);
@@ -28,7 +26,7 @@ public class ResultScene : MonoBehaviour
         //else losing it will say your fired and lose image will activate instead
         else
         {
-            resultBgm.Play();
+
             resultText.text = "YOU'RE FIRED!";
             resultText.color = loseRed;
             winImage.SetActive(false);
